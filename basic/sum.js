998eeaed0a06d3766c3d89fbe7df7b4e55753efd
tdhuan <dinhhuantran3010@gmail.com>
2023-01-29 16:22:47 +0700

    Add babel to tranform code

--- basic/sum.js
+++ basic/sum.js
@@ -1,4 +1,8 @@
-function sum(a, b) {
-  return a + b;
+// function sum(a, b) {
+//   return a + b;
+// }
+// module.exports = sum;
+
+export const sum = (a,b) => {
+  return a + b
 }
-module.exports = sum;
